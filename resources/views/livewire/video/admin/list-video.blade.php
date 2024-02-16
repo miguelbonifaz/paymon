a9c6cd80286df620cf7ae106770f8301283c06ec
miguelbonifaz <miguelbonifaz126@gmail.com>
2024-02-15 23:44:45 -0500

    Column link was removed

--- resources/views/livewire/video/admin/list-video.blade.php
+++ resources/views/livewire/video/admin/list-video.blade.php
@@ -15,7 +15,6 @@
                         <thead class="bg-gray-50">
                         <tr>
                             <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">Name</th>
-                            <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Link</th>
                             <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Views</th>
                         </tr>
                         </thead>
@@ -25,7 +24,6 @@
                                 <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
                                     {{ $video->title }}
                                 </td>
-                                <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ $video->url }}</td>
                                 <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
                                     {{ $video->views }}
                                 </td>
